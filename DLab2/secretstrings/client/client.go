2104d2287c3d45c1d8fb01bffd709c2c53d519fb
0xFaded <xav@bracket.software>
2024-10-03 14:10:55 +0100

    finished secret strings

--- DLab2/secretstrings/client/client.go
+++ DLab2/secretstrings/client/client.go
@@ -1,16 +1,71 @@
 package main
+
 import (
-//	"net/rpc"
+	"bufio"
 	"flag"
-//	"bufio"
-//	"os"
-//	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
+	"net/rpc"
+	"os"
+
+	//	"bufio"
+	//	"os"
 	"fmt"
+
+	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
 )
 
-func main(){
-	server := flag.String("server","127.0.0.1:8030","IP:port string to connect to as server")
-	flag.Parse()
+
+
+func callReverse(rpc *rpc.Client) {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		input, _ := reader.ReadString('\n')
+		request := stubs.Request{Message: input}
+		response := new(stubs.Response)
+		err := rpc.Call(stubs.ReverseHandler, request, response)
+		if err != nil {
+			fmt.Println("Error calling reverse handler:", err)
+			return
+		}
+		fmt.Println("Server responded with:", response.Message)
+	}
+}
+
+func callPremiumReverse(rpc *rpc.Client) {
+	reader := bufio.NewReader(os.Stdin)
+	for {
+		input, _ := reader.ReadString('\n')
+		request := stubs.Request{Message: input}
+		response := new(stubs.Response)
+		err := rpc.Call(stubs.PremiumReverseHandler, request, response)
+		if err != nil {
+			fmt.Println("Error calling premium reverse handler:", err)
+			return
+		}
+		fmt.Println("Server responded with:", response.Message)
+	}
+}
+
+func main() {
+	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect to as server")
+
 	fmt.Println("Server: ", *server)
 	//TODO: connect to the RPC server and send the request(s)
+	callType := flag.String("type", "standard", "Type of call to make (standard/premium)")
+	flag.Parse()
+
+	rpc, err := rpc.Dial("tcp", *server)
+	if err != nil {
+		fmt.Println("Error dialing:", err)
+		return
+	}
+	
+	if *callType == "standard" {
+		callReverse(rpc)
+	} else if *callType == "premium" {
+		callPremiumReverse(rpc)
+	} else {
+		fmt.Println("Invalid type input please use \"standard\" or \"premium\"")
+		return
+	}
+
 }
--- DLab2/secretstrings/server/server.go
+++ DLab2/secretstrings/server/server.go
@@ -1,23 +1,61 @@
-
 package main
+
 import (
-//	"errors"
-//	"flag"
-//	"fmt"
-//	"net"
-	"time"
+	//	"errors"
+	//	"flag"
+	//	"fmt"
+	//	"net"
+	"flag"
+	"fmt"
 	"math/rand"
-//	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
-//	"net/rpc"
+	"net"
+	"net/rpc"
+	"time"
+	"uk.ac.bris.cs/distributed2/secretstrings/stubs"
+	// "net/rpc"
 )
 
 /** Super-Secret `reversing a string' method we can't allow clients to see. **/
 func ReverseString(s string, i int) string {
-    time.Sleep(time.Duration(rand.Intn(i))* time.Second)
-    runes := []rune(s)
-    for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-        runes[i], runes[j] = runes[j], runes[i]
-    }
-    return string(runes)
+	time.Sleep(time.Duration(rand.Intn(i)) * time.Second)
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
 }
 
+func PremiumReverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+type SecretStringOperations struct{}
+
+func (s *SecretStringOperations) Reverse(req stubs.Request, res *stubs.Response) (err error) {
+	res.Message = ReverseString(req.Message, 10)
+	return
+}
+
+func (s *SecretStringOperations) FastReverse(req stubs.Request, res *stubs.Response) (err error) {
+	res.Message = PremiumReverseString(req.Message)
+	return
+
+}
+
+func main() {
+	portPtr := flag.String("port", ":8030", "port to listen on")
+	flag.Parse()
+
+    rpc.Register(&SecretStringOperations{})
+	ln, err := net.Listen("tcp", *portPtr)
+	if err != nil {
+		fmt.Println("error creating listener on port", *portPtr)
+	}
+	defer ln.Close()
+
+    rpc.Accept(ln)
+}
