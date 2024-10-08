558742b5f430b96dbbb5d733bda3e130642bc889
0xFaded <xav@bracket.software>
2024-10-08 20:03:03 +0100

    fixing quiz file to improve readability and add comments

--- lab5/quiz/quiz.c
+++ lab5/quiz/quiz.c
@@ -14,16 +14,22 @@ typedef struct question question_t;
 struct ask_args
 {
     int score;
+    // conditional thread to signal when questions are done
     pthread_cond_t *cond_thread;
+    // mutex to protect score and finished flag
     pthread_mutex_t *mutex;
+    // flag to signal when questions are done
     bool finished;
 };
 typedef struct ask_args ask_args_t;
 
 struct timer_args
-{
+{   
+    // conditional thread to signal when time is up
     pthread_cond_t *cond_thread;
+    // mutex to protect finished flag
     pthread_mutex_t *mutex;
+    // flag to signal when time is up
     bool finished;
 };
 typedef struct timer_args timer_args_t;
@@ -32,11 +38,14 @@ void *timer(void *args)
 {
     timer_args_t *timer_args = args;
     sleep(5);
+
+    // signal that time is up
     pthread_mutex_lock(timer_args->mutex);
     timer_args->finished = true;
     pthread_cond_signal(timer_args->cond_thread);
     pthread_mutex_unlock(timer_args->mutex);
-    printf("5s have passed\n");
+
+    printf("\n5s have passed\n");
     pthread_exit(NULL);
 }
 
@@ -65,19 +74,20 @@ void *ask(void *args)
 
         if (!strcmp(input, answer))
         {
-            printf("Correct!\n");
+            // increment score
             pthread_mutex_lock(ask_args->mutex);
             ask_args->score++;
             pthread_mutex_unlock(ask_args->mutex);
+
+            printf("Correct!\n");
         }
-        else
+        else if (strcmp(input, answer))
         {
             printf("Incorrect :-(\n");
         }
-        pthread_mutex_lock(ask_args->mutex);
-        pthread_cond_signal(ask_args->cond_thread);
-        pthread_mutex_unlock(ask_args->mutex);
     }
+    // signal that questions are done
+    pthread_cond_signal(ask_args->cond_thread);
     ask_args->finished = true;
 }
 
@@ -106,21 +116,23 @@ int main(int argc, char const *argv[])
         printf("Error creating asker thread.\n");
     }
 
+    // wait for questions or timer to finish
     while (!ask_args.finished && !timer_args.finished)
     {
-        printf("waiting");
         pthread_cond_wait(&cond_thread, &mutex);
     };
 
     if (timer_args.finished)
-    {
         printf("Time's up!\n");
-        pthread_exit(NULL);
-        return 0;
-    }
-    score = ask_args.score;
-    pthread_exit(NULL);
 
+    score = ask_args.score;
     printf("End of questions, final score %d\n", score);
+
+    // cleanup
+    pthread_cancel(timer_thread);
+    pthread_join(timer_thread, NULL);
+    pthread_mutex_destroy(&mutex);
+    pthread_cond_destroy(&cond_thread);
+
     return 0;
 }
